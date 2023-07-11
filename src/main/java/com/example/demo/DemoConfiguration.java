2ddaff1b2b8609ffd77682baf21b0f555c2628b3
petar.tahchiev <paranoiabla@gmail.com>
2023-07-11 18:05:39 +0300

    Remove the flter

--- src/main/java/com/example/demo/DemoConfiguration.java
+++ src/main/java/com/example/demo/DemoConfiguration.java
@@ -3,14 +3,12 @@ package com.example.demo;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestProperties;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableConfigurationProperties(value = { RepositoryRestProperties.class })
 @EntityScan({ "com.example.demo.entity.**" })
-@EnableJpaRepositories(basePackages = { "com.example.demo.repository.jpa" }, includeFilters = @ComponentScan.Filter(value = DemoRepositoryIncludeFilter.class, type = FilterType.CUSTOM), repositoryFactoryBeanClass = DemoJpaRepositoryFactoryBean.class)
+@EnableJpaRepositories(basePackages = { "com.example.demo.repository.jpa" }, repositoryFactoryBeanClass = DemoJpaRepositoryFactoryBean.class)
 public class DemoConfiguration {
 }
